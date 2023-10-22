845a7ac2bd92339ec8a158d507401981750cb7f8
= <=>
2023-10-22 12:53:03 +0300

    амеба 2 алгоритм

--- Ameba.cs
+++ Ameba.cs
@@ -14,26 +14,39 @@ public static class Ameba
           через 12 часов - 16 амеб
 
         Алгоритм (от лица программиста)
-        1. Создать амебу
-        2. Начать цикл для симулиции времени
-        3. Если прошло 3 часа, удвоить количество амеб
-         3.1. Показать количество амеб
-        4. Если прошло 24 часа, то завершить цикл.
-        5. Повторить цикл
+        1. Создать переменную для хранения количества клеток в организме(kletkiVOrganizme)
+        2. Создать переменную для хранения колчиства клеток в организме за каждые 3 часа (kletkiVOrganizmeKajdie3Chasa)
+        3. Создать переменну для хранения времен удвоения(vremenaUdvoenie)           
+        4. Начать цикл для симулиции времени(time)
+        5. Если прошло 3 часа, 
+            5.1 Удвоить kletkiVOrganizme
+            5.2. Показать kletkiVOrganizme за time
+            5.3. Добавить kletkiVOrganizme в массив kletkiVOrganizmeKajdie3Chasa
+            5.4  Добавить time в массив vremenaUdvoenie
+
+        6. Если прошло 24 часа, то завершить цикл.
+        7. Повторить цикл
+        8. Найти индекс 6 внутри vremenaUdvoenie (indexVremeni)
+        9. Найти kletkiVOrganizme в kletkiVOrganizmeKajdie3Chasa по indexVremeni
+        10. Показать kletkiVOrganizme
         */
 
-        int ameba = 1;
+        int kletkiVOrganizme = 1;
 
         int time = 0;
         // Одно итерация равна одному часу.
+
+        int[] kletkiVOrganizmeKajdie3Chasa;//1, 2, 4, 8, 16, 32...
+        int[] vremenaUdvoenie;             //0, 3, 6, 9, 12, 15, 18...
+
         while (true)
         {
             time += 1;
 
             if (time % 3 == 0)
             {
-                ameba *= 2;
-                Console.WriteLine($"Время : {time}, количесвто амеб: {ameba}");
+                kletkiVOrganizme *= 2;
+                Console.WriteLine($"Время : {time}, количесвто амеб: {kletkiVOrganizme}");
 
             }
             if (time == 24)
@@ -42,6 +55,7 @@ public static class Ameba
             }
         }
 
+        Console.WriteLine(/*Количество клеток через 6 часов*/);
 
 
     }
